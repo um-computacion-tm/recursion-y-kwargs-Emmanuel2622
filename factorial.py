490e2a0484060a7c6848b0246b52cbd60ef264f9
emmaa.den-um <emma26228@gmail.com>
2024-04-20 21:53:23 -0300

    Creacion de la funcion factroial

---
+++ factorial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    resultado = n * factorial(n-1)
+    print("-" *30)
+    print("El factorial de ", n, " == ",  resultado)
+    return resultado
+    
