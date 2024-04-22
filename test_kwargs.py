ce7bac72421993e0e8f6944b688e8f05eecaf525
emmaa.den-um <emma26228@gmail.com>
2024-04-22 15:27:38 -0300

    Upgrade

--- test_kwargs.py
+++ test_kwargs.py
@@ -6,21 +6,21 @@ class test_kwargs(unittest.TestCase):
     
     def test_1(self):
         result = buscador_datos("Pablo", "Diego", "Maradona", "Messi", **db)
-        self.assertAlmostEqual(result, {'Persona 0': {'primer_nombre': 'Pablo', 'segundo_nombre': 'Diego', 'primer_apellido': 'Maradona', 'segundo_apellido': 'Messi'}})
+        self.assertEqual(result, {'Persona 0': {'primer_nombre': 'Pablo', 'segundo_nombre': 'Diego', 'primer_apellido': 'Maradona', 'segundo_apellido': 'Messi'}})
 
     def test_2(self):
         result = buscador_datos("Jose", "Lucas", "Mosca", "Nono", **db)
-        self.assertAlmostEqual(result, {'Persona  1': {'primer_nombre': 'Jose', 'segundo_nombre': 'Lucas', 'primer_apellido': 'Mosca', 'segundo_apellido': 'Nono'}})
+        self.assertEqual(result, {'Persona  1': {'primer_nombre': 'Jose', 'segundo_nombre': 'Lucas', 'primer_apellido': 'Mosca', 'segundo_apellido': 'Nono'}})
 
     def test_3(self):
         result = buscador_datos("Jose", **db)
-        self.assertAlmostEqual(result, {})
+        self.assertEqual(result, {})
 
     def test_4(self):
         result = buscador_datos("Jose", "Lucas", "Maradona", "Messi", **db)
-        self.assertAlmostEqual(result, {})
+        self.assertEqual(result, {})
 
     def test_5(self):
         result = buscador_datos("Jose", "Pedro", "Rodriguez", "Luca", **db)
-        self.assertAlmostEqual(result, {'Persona  2': {'primer_nombre': 'Jose', 'segundo_nombre': 'Pedro', 'primer_apellido': 'Rodriguez', 'segundo_apellido': 'Luca'}})
+        self.assertEqual(result, {'Persona  2': {'primer_nombre': 'Jose', 'segundo_nombre': 'Pedro', 'primer_apellido': 'Rodriguez', 'segundo_apellido': 'Luca'}})
 unittest.main()
