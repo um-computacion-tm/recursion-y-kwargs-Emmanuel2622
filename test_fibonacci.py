6a31ac3ea532b685708003877f0115969a23b214
emmaa.den-um <emma26228@gmail.com>
2024-04-22 15:30:04 -0300

    Upgrade

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -4,55 +4,55 @@ from fibonacci import fibonacci
 class TestFibonacci(unittest.TestCase):
     def test_0(self):
         resultado = fibonacci(0)
-        self.assertAlmostEqual(resultado, 0)
+        self.assertEqual(resultado, 0)
 
 class TestFibonacci(unittest.TestCase):
     def test_1(self):
         resultado = fibonacci(1)
-        self.assertAlmostEqual(resultado, 1)
+        self.assertEqual(resultado, 1)
 
 class TestFibonacci(unittest.TestCase):
     def test_2(self):
         resultado = fibonacci(2)
-        self.assertAlmostEqual(resultado, 1)
+        self.assertEqual(resultado, 1)
 
 class TestFibonacci(unittest.TestCase):
     def test_3(self):
         resultado = fibonacci(4)
-        self.assertAlmostEqual(resultado, 3)
+        self.assertEqual(resultado, 3)
 
 class TestFibonacci(unittest.TestCase):
     def test_4(self):
         resultado = fibonacci(5)
-        self.assertAlmostEqual(resultado, 5)
+        self.assertEqual(resultado, 5)
 
 class TestFibonacci(unittest.TestCase):
     def test_5(self):
         resultado = fibonacci(6)
-        self.assertAlmostEqual(resultado, 8)
+        self.assertEqual(resultado, 8)
 
 class TestFibonacci(unittest.TestCase):
     def test_6(self):
         resultado = fibonacci(7)
-        self.assertAlmostEqual(resultado, 13)
+        self.assertEqual(resultado, 13)
 
 class TestFibonacci(unittest.TestCase):
     def test_7(self):
         resultado = fibonacci(8)
-        self.assertAlmostEqual(resultado, 21)
+        self.assertEqual(resultado, 21)
 
 class TestFibonacci(unittest.TestCase):
     def test_8(self):
         resultado = fibonacci(9)
-        self.assertAlmostEqual(resultado, 34)
+        self.assertEqual(resultado, 34)
 
 class TestFibonacci(unittest.TestCase):
     def test_9(self):
         resultado = fibonacci(10)
-        self.assertAlmostEqual(resultado, 55)
+        self.assertEqual(resultado, 55)
 
 class TestFibonacci(unittest.TestCase):
     def test_10(self):
         resultado = fibonacci(11)
-        self.assertAlmostEqual(resultado, 89)
+        self.assertEqual(resultado, 89)
 unittest.main()
